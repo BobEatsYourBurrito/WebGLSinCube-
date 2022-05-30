ce8281e18c65f1e05df7d9642faf2fc06f990c47
Bo88y <45578245+BobEatsYourBurrito@users.noreply.github.com>
2022-05-30 17:09:43 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+let xoff = 1;
+let w, r, g, b, wi;
+let maxD;
+let densitySlider, speedSlider, rSlider, gSlider, bSlider, offsetSlider, lights;
+
+function setup() {
+  createCanvas(1000,1000,WEBGL);
+  maxD = dist(0,0, 250, 250);
+  callSliders();
+
+}
+
+function draw() {
+  background(175);
+  assignSliders();
+  ortho(-400, 400, -400, 400, -500, 2000);
+  ambientLight(light, light, light, 0, -1, 0);
+
+  translate(0, -150, -500);
+  rotateX(-PI / 8);
+  rotateY(PI / 4);
+
+  let mousex = mouseX - width / 2;
+  let mousey = mouseY - height / 2;
+
+  // fill(255);
+  rectMode(CENTER);
+  for(let j = 0; j < 500; j += w) {
+    for(let i = 0; i < 500; i += w) {
+      push();
+      //let d = dist(i - mousex, j - mousey ,250,250); //origin to mouse
+      let d = dist(i, j,250,250);
+      let d1 = dist(i ,j ,250,250);
+      let offset = map(d, 0, maxD, -wi , wi);
+      let bright = map(d1, 0, maxD, 255, 0);
+      let bright1 = map(d1, 0, maxD, 100, 0);
+      translate((-width /2 + 13) + i,0,j);
+      let h = floor(map(sin(-xoff * 2 + offset),-1,1,25,150));
+      ambientMaterial((bright + r)/2, (bright + g)/2, (bright + b)/2, 255);
+      box(w, h, w);
+      pop();
+    }
+  }
+
+  xoff += speedSlider.value();
+}
---
+++ sliders.js
@@ -0,0 +1,27 @@
+function callSliders() {
+  densitySlider = createSlider(5, 75, 25, 5);
+  speedSlider = createSlider(0.01, 0.1, 0.03, 0.01);
+  rSlider = createSlider(0,255,255,5);
+  gSlider = createSlider(0,255,255,5);
+  bSlider = createSlider(0,255,255,5);
+  offsetSlider = createSlider(1,5,1,0.5);
+  lightSlider = createSlider(0,255,255,5);
+
+  densitySlider.position(1000, 99);
+  speedSlider.position(1000, 119);
+  rSlider.position(1000, 139);
+  gSlider.position(1000, 159);
+  bSlider.position(1000, 179);
+  lightSlider.position(1000, 199);
+  offsetSlider.position(1000, 219);
+
+}
+
+function assignSliders() {
+    w = densitySlider.value();
+    r = rSlider.value();
+    g = gSlider.value();
+    b = bSlider.value();
+    wi = offsetSlider.value();
+    light = lightSlider.value();
+  }
